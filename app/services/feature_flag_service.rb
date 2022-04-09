688cd8b5530ecda24d82bc89c2ad82ccfc7cc0b4
compwron <compwron@users.noreply.github.com>
2022-04-09 12:03:25 -0700

    wip mess with weird db migration integer change

--- app/services/feature_flag_service.rb
+++ app/services/feature_flag_service.rb
@@ -1,6 +1,7 @@
 class FeatureFlagService
   SOME_FLAG = "some_flag"
   SHOW_ADDITIONAL_EXPENSES_FLAG = "show_additional_expenses"
+  PRINCE_GEORGE_FUND_REQUEST = "prince_george_fund_request"
 
   def self.is_enabled?(feature_flag_name)
     FeatureFlag.find_by(name: feature_flag_name)&.enabled
